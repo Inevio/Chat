bc226b1c3435ed923b9e663947f5d89b6e2cee04
Javier Garmón <jgarmon@weezeel.com>
2014-05-17 01:57:47 +0200

    Chat - New STUN and TURN servers

--- static/webrtc.js
+++ static/webrtc.js
@@ -1,30 +1,23 @@
-    configuration      = { "iceServers": [
-                                    {
-                                        url: 'turn:numb.viagenie.ca',
-                                        credential: 'mangakas123',
-                                        username: 'rrojot@hotmail.com'
-                                    },
-                                    {
-                                        url: 'turn:192.158.29.39:3478?transport=udp',
-                                        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                                        username: '28224511:1379330808'
-                                    },
-                                    {
-                                        url: 'turn:192.158.29.39:3478?transport=tcp',
-                                        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                                        username: '28224511:1379330808'
-                                    },
-                                    {
-                                        url: 'turn:192.158.30.23:3478?transport=udp',
-                                        credential: 'RaebIwHQPUmabKiySrCr8kbHtSg=',
-                                        username: '1394128738:02043197'
-                                    },
-                                    {
-                                        url: 'turn:192.158.30.23:3478?transport=tcp',
-                                        credential: 'RaebIwHQPUmabKiySrCr8kbHtSg=',
-                                        username: '1394128738:02043197'
-                                    }
-                                 ]};
+
+configuration = {
+
+    iceServers: [
+
+        { url : 'stun:stun.l.google.com:19302' },
+        { url : 'stun:stun1.l.google.com:19302' },
+        { url : 'stun:stun2.l.google.com:19302' },
+        { url : 'stun:stun3.l.google.com:19302' },
+        { url : 'stun:stun4.l.google.com:19302' },
+
+        {
+            
+            url        : 'turn:5.135.159.85', // To Do -> Es la IP de la beta, ponerle el nombre del dominio
+            username   : 'test',              // To Do -> Datos muy vulnerables
+            credential : 'test'
+
+        }
+
+]};
 
 RTCPeerConnection      = typeof webkitRTCPeerConnection === 'undefined' ? mozRTCPeerConnection : webkitRTCPeerConnection;
 navigator.getUserMedia = navigator.getUserMedia  || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
